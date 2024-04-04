795c7522f3c05105a3aa0bb7ee928cd3a8effa34
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-04 15:01:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,32 @@ from streamlit_keplergl import keplergl_static
 from keplergl import KeplerGl
 from html import escape
 
+import google.generativeai as genai
+import requests
+
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+
+safe = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
 def wide_space_default():
   st.set_page_config(layout="wide")
 
@@ -66,4 +92,6 @@ with col2:
   # Add user message to the chat
   if user_input:
     add_message("User", user_input)
+
+    
     add_message("assistant", f"echo {user_input}")
