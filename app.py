08708f9d4f08a809267b73ce81434317644ad2f4
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-04 08:16:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 from streamlit_keplergl import keplergl_static
 from keplergl import KeplerGl
 
-st.write("This is a kepler.gl map in streamlit")
+st.write("Complex Research, Made Easy")
 
 map_1 = KeplerGl(height=400)
 
 col1, col2 = st.columns([3, 1])
 with col1:
   keplergl_static(map_1)
+with col2:
+  st.header("Chat with WEN-OKN")
