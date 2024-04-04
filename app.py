f23472760085476c1c02cee7c692c7ab5fb9c537
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-03 23:11:17 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from streamlit_keplergl import keplergl_static
+from keplergl import KeplerGl
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "City": ["San Francisco", "San Jose", "Palo Alto"],
+        "Latitude": [37.77, 37.33, 37.44],
+        "Longitude": [-122.43, -121.89, -122.14],
+    }
+)
+
+st.dataframe(df.head())
+
+st.write("This is a kepler.gl map with data input in streamlit")
+
+map_1 = KeplerGl(height=400)
+map_1.add_data(
+    data=df, name="cities"
+)  # Alternative: KeplerGl(height=400, data={"name": df})
+
+keplergl_static(map_1, center_map=True)
