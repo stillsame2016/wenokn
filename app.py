044348ac19c2f62271e165684e91d7e74ea52c05
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-03 23:18:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,3 @@ st.write("This is a kepler.gl map in streamlit")
 
 map_1 = KeplerGl(height=400)
 keplergl_static(map_1)
-
-map_1 = KeplerGl(height=400)
-map_1.add_data(
-    data=df, name="cities"
-)  # Alternative: KeplerGl(height=400, data={"name": df})
-
-keplergl_static(map_1, center_map=True)
