b703bccafea996aae1f70e793ca60a31c08bed8c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-04 14:48:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ with col2:
   def add_message(sender, message):
     with messages_container:
       # st.chat_message(sender).write(message)
-      st.markdown(f'<div class="message">{ message }</div>', unsafe_allow_html=True)
+      st.markdown(f'<div style="font-size:11pt !important;">{ message }</div>', unsafe_allow_html=True)
  
   # Get user input
   user_input = st.chat_input("What can I help you with?")
