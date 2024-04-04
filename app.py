7b63dfe7f0d59d7639a8afdd66414f3c9bc0a76e
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-04 07:56:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ st.dataframe(df.head())
 st.write("This is a kepler.gl map with data input in streamlit")
 
 map_1 = KeplerGl(height=400)
+keplergl_static(map_1, center_map=True)
 map_1.add_data(
     data=df, name="cities"
 )  # Alternative: KeplerGl(height=400, data={"name": df})
-
-keplergl_static(map_1, center_map=True)
