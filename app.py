78d379aaa365741c9844216c2b4340268a79444e
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-05 12:03:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -93,13 +93,12 @@ with col2:
                                      
             response = requests.get(f"https://sparcal.sdsc.edu/staging-api/v1/Utility/wenokn?query_text={message}")
             data = response.text
-            print('-'*70)
-            print(data)
-            if data.startswith('"```sparql'):
-              data = data.split("\n", 1)[1].rsplit("\n", 1)[0] 
-              st.markdown(1000)
-            else:
-              st.markdown(2000)
+            st.write(f"data.startswith('"```sparql'): {data.startswith('"```sparql')}")
+            # if data.startswith('"```sparql'):
+            #   data = data.split("\n", 1)[1].rsplit("\n", 1)[0] 
+            #   st.markdown(1000)
+            # else:
+            #   st.markdown(2000)
             st.code(data)
             time.sleep(10)
       else: 
