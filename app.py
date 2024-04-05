db10e286528d398bfdbf177676404c87103f3216
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-05 10:37:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -100,9 +100,10 @@ with col2:
                                   Identified potential relevant concepts and currently crafting a query. 
                                   Your patience is appreciated as we work on this task.
                                 """):  
+                   st.markdown("**Potential Relevant Entities:**")
                    for concept in concepts:
                        if concept['is_relevant']:
-                           st.markdown(f"{concept['entity']}")
+                           st.markdown(f"- {concept['entity']}")
                    time.sleep(10)
       else: 
          st.chat_message(sender).write(message)
