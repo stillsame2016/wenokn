0232317babd22ddcc8220c72c0c8a056e9139857
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-06 12:12:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,8 @@ with col2:
             map_2.config = config
             map_2.add_data(data=df, name="cities") 
 
-            col1.empty()
-            col1.write("hello")
+            col1.empty(sparql_query)
+            col1.write()
             # with col1:
             #   keplergl_static(map_2)
 
