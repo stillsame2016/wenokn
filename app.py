1b9352536535eeb0a839ad819ae6ae2e2c09896b
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-06 16:04:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,11 @@ map_1.config = config
  
 col1, col2 = st.columns([6, 4])
 with col1:  
-  keplergl_static(map_1)
+  f"""
+     <div id='test'>
+     { keplergl_static(map_1) }
+     </div>
+  """
 
 with col2:
   # if prompt := st.chat_input("What can I help you with?"):
