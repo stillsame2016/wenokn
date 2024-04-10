3f200abbcb76ba6583c43fe4f812f6e3624a419b
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-10 08:37:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -67,13 +67,13 @@ def df_to_gdf(df):
   column_name_parts.pop()
   gdf.attrs['data_name'] = " ".join(column_name_parts).capitalize()
   
-  for column_name in column_names:
-    tmp_column_name_parts = get_column_name_parts(column_name)
-    name = tmp_column_name_parts.pop()  
-    tmp_data_name = " ".join(column_name_parts).capitalize()
-    if tmp_data_name == gdf.attrs['data_name']:
-      gdf.rename(columns={column_name: name}, inplace=True)
-  return gdf
+  # for column_name in column_names:
+  #   tmp_column_name_parts = get_column_name_parts(column_name)
+  #   name = tmp_column_name_parts.pop()  
+  #   tmp_data_name = " ".join(column_name_parts).capitalize()
+  #   if tmp_data_name == gdf.attrs['data_name']:
+  #     gdf.rename(columns={column_name: name}, inplace=True)
+  # return gdf
 
 
 wide_space_default()
