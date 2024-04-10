be8f6eb5b5e6778fe75d31c4be56c0867a083e50
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-04-10 08:14:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,8 @@ def df_to_gdf(df):
     if gdf.attrs['data_name'] == tmp_data_name:
       column_name_parts = get_column_name_parts(column_name)
       gdf.rename(columns={column_name: column_name_parts[-1]}, inplace=True)
+    else:
+      gdf.rename(columns={column_name: tmp_data_name + "_" + column_name_parts[-1]}, inplace=True)
   return gdf
 
 
