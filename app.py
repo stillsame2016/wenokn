c3a63d1c0398f095862065d37fcf54e60d46172c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-10-30 14:55:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,6 @@ Groq_KEY_2 = st.secrets["Groq_KEY_2"]
 OpenAI_KEY = st.secrets["OpenAI_KEY"]
 SDSC_KEY = st.secrets["SDSC_KEY"]
 
-st.code(SDSC_KEY)
-
 # llm = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY)
 # llm2 = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY_2)
 
@@ -40,6 +38,8 @@ llm2 = ChatOpenAI(model="gpt-4o", temperature=0, max_tokens=5000, api_key=OpenAI
 
 # Set the wide layout of the web page
 st.set_page_config(layout="wide", page_title="WEN-OKN")
+
+st.code(SDSC_KEY)
 
 # Set up the title
 st.markdown("### &nbsp; WEN-OKN: Dive into Data, Never Easier")
